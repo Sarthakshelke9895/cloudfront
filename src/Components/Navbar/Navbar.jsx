2769d9a8965c540a68e56491787fc83666c9b7a3
Dell <sarthak shelke>
2025-08-15 20:20:58 +0530

    Location Reload on Navabr click and Updating backend urls

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,10 @@ import Logo from '../../Assests/weblogo.PNG';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = ({ toggleMenu, isMenuOpen }) => {
+  const handlereload =()=>{
+    window.location.reload(); // Reloads the page
+
+  }
   return (
     <div className="nav">
       {/* Hamburger on the left (only visible on mobile via CSS) */}
@@ -12,7 +16,7 @@ const Navbar = ({ toggleMenu, isMenuOpen }) => {
       </div>
 
       {/* Logo and name centered */}
-      <div className="img_and_logo">
+      <div onClick={handlereload} className="img_and_logo">
         <img src={Logo} alt="App Logo" />
         <h1>XanaX</h1>
       </div>
